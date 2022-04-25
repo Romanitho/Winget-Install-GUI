2fcfa70554b9e93615ced36a81bb02df38c69e8b
Romain <96626929+Romanitho@users.noreply.github.com>
2022-04-25 20:54:19 +0200

    Delete Run.bat

--- Sources/Run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File; Start-Process powershell.exe -Argument '-noprofile -executionpolicy bypass -file """%~dp0Winget-Install-GUI.ps1"" '" -Verb RunAs
