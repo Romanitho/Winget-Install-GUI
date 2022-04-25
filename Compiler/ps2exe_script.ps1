43610d274bc8cfe77017360c8e658cbe108b33be
Romain <96626929+Romanitho@users.noreply.github.com>
2022-04-25 21:02:09 +0200

    Add files via upload

---
+++ Compiler/ps2exe_script.ps1
@@ -0,0 +1,7 @@
+﻿<### Install ps2exe ###>
+
+Install-Module ps2exe
+
+<### Run ps2exe ###>
+
+Invoke-ps2exe -inputFile ".\Winget-Install-GUI.ps1" -outputFile ".\WiGui.exe" -noConsole -requireAdmin -title "WiGui" -version 1.1.0 -copyright "Romanitho" -product "WiGui"
