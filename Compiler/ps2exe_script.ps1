8a442130cff8e4f8d89115fbf0fce3228d9c151c
Romain <96626929+Romanitho@users.noreply.github.com>
2022-04-27 03:39:22 +0200

    Update ps2exe_script.ps1

--- Compiler/ps2exe_script.ps1
+++ Compiler/ps2exe_script.ps1
@@ -1,11 +1,11 @@
 ﻿<### Install ps2exe ###>
-
-Install-Module ps2exe
+#Install-Module ps2exe
 
 <### Run ps2exe ###>
-$InputFile = ".\Sources\Winget-Install-GUI.ps1"
-$OutputFile = ".\Sources\WiGui.exe"
-$Icon = ".\Sources\WiGui.ico"
+$Path = Split-Path $PSScriptRoot -Parent
+$InputFile = "$Path\Sources\Winget-Install-GUI.ps1"
+$OutputFile = "$Path\Sources\WiGui.exe"
+$Icon = "$Path\Sources\WiGui.ico"
 $Title = "WiGui"
 $AppVersion = "1.3.0"
 Invoke-ps2exe -inputFile $InputFile -outputFile $OutputFile -noConsole -requireAdmin -title $Title -version $AppVersion -copyright "Romanitho" -product $Title -icon $Icon
