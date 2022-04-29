ab54cff6e39fb2ca310c62270135e59460bb1f2b
Romain <96626929+Romanitho@users.noreply.github.com>
2022-04-29 19:47:06 +0200

    Update ps2exe_script.ps1

--- Compiler/ps2exe_script.ps1
+++ Compiler/ps2exe_script.ps1
@@ -8,4 +8,4 @@ $OutputFile = "$Path\Sources\WiGui.exe"
 $Icon = "$Path\Sources\WiGui.ico"
 $Title = "WiGui"
 $AppVersion = "1.4.0"
-Invoke-ps2exe -inputFile $InputFile -outputFile $OutputFile -noConsole -requireAdmin -title $Title -version $AppVersion -copyright "Romanitho" -product $Title -icon $Icon
+Invoke-ps2exe -inputFile $InputFile -outputFile $OutputFile -noConsole -requireAdmin -title $Title -version $AppVersion -copyright "Romanitho" -product $Title -icon $Icon -noerror
